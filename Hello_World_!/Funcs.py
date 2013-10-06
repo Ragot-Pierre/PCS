9cd8e531b31c929a4766827e19c6a3a704a95ca8
Ragot Pierre <pierre-ragot@hotmail.fr>
2013-10-06 03:31:28 +0200

    First upload, some basics !

---
+++ Hello_World_!/Funcs.py
@@ -0,0 +1,10 @@
+﻿# -*- coding: utf-8 -*-
+
+def say_hello(user):
+	print("Hello " + user + " !")
+
+def say_goodbye(user):
+	print("Goodbye " + user + " !")
+
+def say_what():
+	print("What ?")
---
+++ Hello_World_!/Main.py
@@ -0,0 +1,20 @@
+﻿# -*- coding: utf-8 -*-
+
+import os
+
+os.chdir("C://Users//Elpollo973//Documents//GitHub//PythonCodeSnippets//Hello_World_!")
+
+from Funcs import *
+
+quit = 0
+user = "you"
+
+while quit != 1:
+	user_message = str(input())
+	if user_message == "Hi":
+		say_hello(user)
+	elif user_message == "Bye":
+		say_goodbye(user)
+		quit = 1
+	else:
+		say_what()
